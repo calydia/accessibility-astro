127542c75afff49c2a93c997b68e65b96b6b3303
Sanna Mäkinen <sanna.makinen@gmail.com>
2023-10-08 12:32:24 +0300

    Remove the sitemap correct url since it's not in use yet

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://a11y.sanna.ninja',
   integrations: [sitemap(), tailwind(), react()]
 });
