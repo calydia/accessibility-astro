482249fab8fa987f9b546c146bf45288384a9376
Sanna Mäkinen <sanna.makinen@gmail.com>
2023-10-08 13:25:59 +0300

    Add the sitemap url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://a11y.sanna.ninja',
   integrations: [sitemap(), tailwind(), react()],
   redirects: {
     '/': {
