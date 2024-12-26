d852027133f406ed2612d52b0eaf8ed6bb8f5ccd
Sanna Kramsi <sanna.kramsi@gmail.com>
2024-12-26 10:44:59 +0200

    Remove unused images, update tsconfig with new recommendations, remove ld redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,11 +18,4 @@ export default defineConfig({
       }
     }
   }), tailwind(), react(), icon()],
-  redirects: {
-    '/fi/perusteet/vammaisuuden-tyypit/psykologiset-psykiatriset-vammat': '/fi/perusteet/vammaisuuden-tyypit/psykologiset-psyykkiset-vammat/',
-    '/en/fundamentals/accessibility-and-universal-design/universal-design': '/en/fundamentals/the-reach-of-accessibility/universal-design/',
-    '/en/fundamentals/accessibility-and-universal-design/universal-design-for-learning': '/en/fundamentals/the-reach-of-accessibility/universal-design-for-learning/',
-    '/fi/perusteet/saavutettavuus-ja-yleissuunnittelu/yleissuunnittelu': '/fi/perusteet/saavutettavuuden-vaikutukset/yleissuunnittelu/',
-    '/fi/perusteet/saavutettavuus-ja-yleissuunnittelu/oppimisen-yleissuunnittelu': '/fi/perusteet/saavutettavuuden-vaikutukset/oppimisen-yleissuunnittelu/'
-  }
 });
